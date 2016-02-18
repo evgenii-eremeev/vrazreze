5410f011fae7f19f9bca6c13f4e26a63b2cd392d
JayCrypto <jaycrypto@gmail.com>
2016-02-18 07:53:01 +0500

    added user model

---
+++ server/models/user.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    username: { type: String, maxlength: 255, required: true },
+    pwd_hash: { type: String, required },
+    register_date: { type: Date, default: Date.now },
+    updated: { type: Date, default: Date.now },
+    rating: { type: Number, default: 0 },
+    education: { type: String, maxlength: 255 },
+    software: { type: String, maxlength: 255 },
+    speciality: { type: String, maxlength: 255 },
+    about: { type: String, maxlength: 1500 },
+    site: { type: String, maxlength: 255 },
+    social: {type: String, maxlength: 255 }
+    other: Schema.Types.Mixed
+});
+
+var User = mongoose.model('User', drawingSchema);
+
+module.exports = User;
