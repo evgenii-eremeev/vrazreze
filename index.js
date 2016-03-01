9897bbe61e09da6c2e16fbee949981d816c421cf
jay <jaycrypto@gmail.com>
2016-03-01 19:23:53 +0500

    adding react

---
+++ index.js
@@ -0,0 +1,10 @@
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+
+// import components
+import App from './app/components/App';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
