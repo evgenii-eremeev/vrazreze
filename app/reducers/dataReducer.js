269b4e33d0a31ad2ae582619925c9360573be2c8
jay <jaycrypto@gmail.com>
2016-03-15 18:45:18 +0500

    added reducers added test

---
+++ app/reducers/dataReducer.js
@@ -0,0 +1,19 @@
+
+
+const defaultStartState = {
+    categories: [],
+    drawings: []
+};
+
+function dataReducer (state = defaultStartState, action) {
+    switch (action.type) {
+        case "FETCH_DRAWINGS":
+            return Object.assign({}, state, { drawings: action.data });
+        case "FETCH_CATEGORIES":
+            return Object.assign({}, state, { categories: action.data });
+        default:
+            return state;
+    }
+}
+
+export default dataReducer;
---
+++ test/dataReducer.spec.js
@@ -0,0 +1,73 @@
+import expect from 'expect';
+
+import dataReducer from '../app/reducers/dataReducer';
+
+describe('dataRuducer', function () {
+
+    describe('default', function () {
+        it('should return default state', function () {
+            const stateBefore = undefined;
+            const stateAfter = {
+                categories: [],
+                drawings: []
+            };
+            expect(
+                dataReducer(stateBefore, {})
+            ).toEqual(stateAfter);
+        });
+    });
+
+    describe('FETCH_DRAWINGS', function () {
+        it('should insert data from action', function () {
+            const stateBefore = {
+                categories: [],
+                drawings: []
+            };
+            const stateAfter = {
+                categories: [],
+                drawings: [
+                    {name: 'drawing1'},
+                    {name: 'drawing2'}
+                ]
+            };
+            const action = {
+                type: 'FETCH_DRAWINGS',
+                data: [
+                    {name: 'drawing1'},
+                    {name: 'drawing2'}
+                ]
+            };
+            expect(
+                dataReducer(stateBefore, action)
+            ).toEqual(stateAfter)
+        });
+    });
+
+    describe('FETCH_CATEGORIES', function () {
+        it('should insert data from action', function () {
+            const stateBefore = {
+                categories: [],
+                drawings: []
+            };
+            const stateAfter = {
+                categories: [
+                    {name: 'cat1'},
+                    {name: 'cat2'}
+                ],
+                drawings: []
+            };
+            const action = {
+                type: 'FETCH_CATEGORIES',
+                data: [
+                    {name: 'cat1'},
+                    {name: 'cat2'}
+                ]
+            };
+            expect(
+                dataReducer(stateBefore, action)
+            ).toEqual(stateAfter)
+        });
+    });
+
+
+}); // end dataReduces
