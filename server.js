a61042ba1ac12670aa2e95f6ac852a9f0c550321
jay <jaycrypto@gmail.com>
2016-04-07 02:40:27 +0500

    prod and dev experiments

--- server.js
+++ server.js
@@ -14,28 +14,31 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 require('dotenv').load();
 
-// ************************************
-// This is the real meat of the example
-// ************************************
-if (process.env.NODE_ENV === 'development') {
-    (function() {
-      // Step 1: Create & configure a webpack compiler
-      var webpack = require('webpack');
-      var webpackConfig = require(process.env.WEBPACK_CONFIG ? process.env.WEBPACK_CONFIG : './webpack.config');
-      var compiler = webpack(webpackConfig);
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    (function webpackHotMiddleware() {
+        // Step 1: Create & configure a webpack compiler
+        var webpack = require('webpack');
+        var webpackConfig = require(process.env.WEBPACK_CONFIG ? process.env.WEBPACK_CONFIG : './webpack.config');
+        var compiler = webpack(webpackConfig);
 
-      // Step 2: Attach the dev middleware to the compiler & the server
-      app.use(require("webpack-dev-middleware")(compiler, {
-        noInfo: true, publicPath: webpackConfig.output.publicPath
-      }));
+        // Step 2: Attach the dev middleware to the compiler & the server
+        app.use(require("webpack-dev-middleware")(compiler, {
+            noInfo: true, publicPath: webpackConfig.output.publicPath
+        }));
 
-      // Step 3: Attach the hot middleware to the compiler & the server
-      app.use(require("webpack-hot-middleware")(compiler, {
-        log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
-      }));
+        // Step 3: Attach the hot middleware to the compiler & the server
+        app.use(require("webpack-hot-middleware")(compiler, {
+            log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
+        }));
     })();
-}
-// ************************************
+
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
