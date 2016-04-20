3d95463217afa81a3c24257a45c9b1df53b328aa
jay <jaycrypto@gmail.com>
2016-04-20 13:08:21 +0500

    webpackHotMiddleware config

--- server.js
+++ server.js
@@ -14,23 +14,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 require('dotenv').load();
 
+
+// webpackHotMiddleware
 if (process.env.NODE_ENV !== 'production') {
-    (function webpackHotMiddleware() {
-        // Step 1: Create & configure a webpack compiler
-        var webpack = require('webpack');
-        var webpackConfig = require(process.env.WEBPACK_CONFIG ? process.env.WEBPACK_CONFIG : './webpack.config');
-        var compiler = webpack(webpackConfig);
-
-        // Step 2: Attach the dev middleware to the compiler & the server
-        app.use(require("webpack-dev-middleware")(compiler, {
-            noInfo: true, publicPath: webpackConfig.output.publicPath
-        }));
-
-        // Step 3: Attach the hot middleware to the compiler & the server
-        app.use(require("webpack-hot-middleware")(compiler, {
-            log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
-        }));
-    })();
+    require('./server/config/webpackHotMiddleware')(app);
 }
 
 // passport config
