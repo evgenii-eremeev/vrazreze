9aeb62f6c2e240d6d551da0d3b212a0008af8ce6
jay <jaycrypto@gmail.com>
2016-04-20 23:24:59 +0500

    configured nodemailer and email-templates

--- server/mail/sendOrder.js
+++ server/mail/sendOrder.js
@@ -7,15 +7,7 @@ const EmailTemplate = require('email-templates').EmailTemplate
 // load auth data
 require('dotenv').load();
 
-const templateDir = path.join(__dirname, 'templates', 'order-email');
-
-const order = new EmailTemplate(templateDir);
-const user = {name: 'Joey'};
-order.render(user, function (err, result) {
-    console.log(result.html);
-})
-
-// config
+// config smtp
 const smtpConfig = {
     host: 'smtp.yandex.ru',
     port: 465,
@@ -29,27 +21,37 @@ const smtpConfig = {
 // create reusable transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport(smtpConfig);
 
-// create template based sender function
-const sendOrderTemplate = transporter.templateSender({
-    subject: 'Пользователь!',
-    text: "Hello, only text",
-    html: `
-        <h1>Hello!</h1>
-        <p>{{user}}</p>
-        <p>{{cart}}</p>
-    `
-}, {
-    to: 'e.i.eremeev@gmail.com',
-    from: `"Заказ" <${process.env.MAIL_ADDRESS}>`,
-});
+// create orderEmail template
+const templateDir = path.join(__dirname, 'templates', 'orderEmail');
+const orderEmail = new EmailTemplate(templateDir);
 
 
-// use template based sender to send a message
 function sendOrder(user, cart) {
-    return sendOrderTemplate({}, {
-        user: JSON.stringify(user),
-        cart: JSON.stringify(cart)
-    });
-}
+    // locals is the container for variables that could be used in template
+    // you could not use locals.user.name in template, just user.name
+    const locals = {
+        user: user,
+        cart: cart
+    };
+    
+    // return a Promise object which resolves to nodemailer 'info' object
+    // info.accepted, info.rejected, info.response, info.envelope, info.messageId
+    // will be resoled in mailRouter
+    return (
+        orderEmail
+            .render(locals)                         // Promise from email-template
+            .then(results => {                      // resolves with results object
+                // console.log('result:', results);
+                return transporter.sendMail({
+                    from: `вразрезе.рф <${process.env.MAIL_ADDRESS}>`,
+                    to: 'e.i.eremeev@gmail.com',
+                    subject: 'Пробный заказ!',
+                    html: results.html
+                })
+            })
+    ); // return
+        
+} // sendOrder
+
 
 module.exports = sendOrder;
