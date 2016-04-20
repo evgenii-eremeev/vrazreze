58d38747f66f2f44c56086db0b75f748c0c822dd
jay <jaycrypto@gmail.com>
2016-04-20 13:11:55 +0500

    small

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 require('dotenv').load();
 
-
 // webpackHotMiddleware
 if (process.env.NODE_ENV !== 'production') {
     require('./server/config/webpackHotMiddleware')(app);
@@ -39,14 +38,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(quickthumb.static(process.cwd() + '/public'));
 
+// inject routes
 require('./server/routes')(app);
 
-// 'any uri, because we use react-router'
 // Should be placed at the very end for the rest 'get' requests to work
 app.get('*', function(req, res) {
     res.sendFile(process.cwd() + '/public/index.html');
 });
-
 
 app.listen(app.get('port'), function () {
     console.log("Express server running at http://localhost:" + app.get('port'));
