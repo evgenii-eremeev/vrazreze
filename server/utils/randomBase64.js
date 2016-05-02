9631362ccde6c656e90579cdb822b7d4c7eebdd9
Jay <jaycrypto@gmail.com>
2016-05-03 01:07:20 +0500

    random 64 base string

---
+++ server/utils/randomBase64.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const base64Url = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+
+function randomBase64(length) {
+    let result = "",
+        randInt;
+    for (let i = 0; i < length; i++) {
+        randInt = Math.floor(Math.random() * 64);
+        result += base64Url.substr(randInt, 1);
+    }
+    return result;
+}
+
+randomBase64.base64Url = base64Url;
+
+module.export = randomBase64;
